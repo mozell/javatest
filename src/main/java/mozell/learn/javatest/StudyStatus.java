83dc9a628026e6fd2abc1899b90453775422767f
mozell <mozellatto@gmail.com>
2022-08-22 23:48:31 +0900

    JUnit5 Assertion

---
+++ src/main/java/mozell/learn/javatest/StudyStatus.java
@@ -0,0 +1,5 @@
+package mozell.learn.javatest;
+
+public enum StudyStatus {
+    DRAFT, STARTED, ENDED
+}
--- src/test/java/mozell/learn/javatest/StudyTest.java
+++ src/test/java/mozell/learn/javatest/StudyTest.java
@@ -2,17 +2,69 @@ package mozell.learn.javatest;
 
 import org.junit.jupiter.api.*;
 
+import java.time.Duration;
+import java.util.function.Supplier;
+
 import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertTimeout;
 
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 class StudyTest {
 
     @Test
-    @DisplayName("첫번째 테스트")
-    public void create_first_test() throws Exception {
-        Study study = new Study();
+    @DisplayName("Assertion 강의 assertEquals")
+    public void create_test_Assertion1() throws Exception {
+        Study study = new Study(-10);
         assertNotNull(study);
         System.out.println("create");
+
+        // 테스트 성공 여부에 관계없이 문자열 연산을 무조건 수행함
+        assertEquals(StudyStatus.DRAFT, study.getStatus(), "스터디를 처음 만들면 상태값이 "+ StudyStatus.DRAFT + "여야 한다.");
+        // 테스트가 실패하는 경우에만 문자열 연산을 실행함
+        assertEquals(StudyStatus.DRAFT, study.getStatus(), () -> "스터디를 처음 만들면 상태값이 "+ StudyStatus.DRAFT + "여야 한다.");
+        // new Supplier<String>() 사용
+        assertEquals(StudyStatus.DRAFT, study.getStatus(), new Supplier<String>() {
+            @Override
+            public String get() {
+                return "스터디를 처음 만들면 상태값이 "+ StudyStatus.DRAFT + "여야 한다.";
+            }
+        });
+
+        assertTrue(study.getLimit() > 0, "스터디 최대 참석 가능 인원은 0보다 커야한다.");
+    }
+
+    @Test
+    @DisplayName("Assertion 강의 assertAll")
+    void create_test_Assertion2() {
+        Study study = new Study(-10);
+
+        assertAll(
+                () -> assertNotNull(study),
+                () -> assertEquals(StudyStatus.DRAFT, study.getStatus(),
+                        () -> "스터디를 처음 만들면 상태값이 " + StudyStatus.DRAFT + "여야 한다."),
+                () -> assertTrue(study.getLimit() > 0, "스터디 최대 참석 가능 인원은 0보다 커야한다.")
+        );
+    }
+
+    @Test
+    @DisplayName("Assertion 강의 assertThrows")
+    void create_test_Assertion3() {
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> new Study(-10));
+        String message = exception.getMessage();
+
+        // 예외의 메시지를 뽑아서, 기대했던 메시지와 같은지 체크
+        assertEquals("limit 는 0보다 커야한다.", message);
+    }
+
+    @Test
+    @DisplayName("Assertion 강의 assertTimeout")
+    void create_test_Assertion4() {
+        //assertTimeoutPreemptively(); //TODO ThreadLocal -> 예상치 못한 결과가 나올 수 있는 점을 유의하여 사용해야 한다.
+        assertTimeout(Duration.ofMillis(100), () -> {
+            new Study(10);
+            Thread.sleep(300);
+        });
+
     }
 
     @Test
