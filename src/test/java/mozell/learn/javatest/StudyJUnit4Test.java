fcf02dc591bad5a6f940354bd1f0b2bbe8a9431d
mozell <mozellatto@gmail.com>
2022-09-02 00:04:21 +0900

    JUnit5 마이그레이션

---
+++ src/test/java/mozell/learn/javatest/StudyJUnit4Test.java
@@ -0,0 +1,23 @@
+package mozell.learn.javatest;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class StudyJUnit4Test {
+
+
+    @Before
+    public void before() {
+        System.out.println("before");
+    }
+
+    @Test
+    public void createTest() {
+        System.out.println("test");
+    }
+
+    @Test
+    public void createTest2() {
+        System.out.println("test2");
+    }
+}
