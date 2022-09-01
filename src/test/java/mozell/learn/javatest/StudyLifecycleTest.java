ff2ca60a1a064f25f5425dea9a25b1cca88b9884
mozell <mozellatto@gmail.com>
2022-09-01 22:41:04 +0900

    JUnit5 junit-platform.properties

--- src/test/java/mozell/learn/javatest/StudyLifecycleTest.java
+++ src/test/java/mozell/learn/javatest/StudyLifecycleTest.java
@@ -4,10 +4,11 @@ import org.junit.jupiter.api.*;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
-@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+//@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class StudyLifecycleTest {
 
+    int value = 1;
 
     // private만 아니면된다. 라이프사이클 추가함으로써 static 빼도된다.
     @BeforeAll
@@ -20,6 +21,7 @@ public class StudyLifecycleTest {
     void test1() {
         Study study = new Study(10);
         System.out.println(this);
+        System.out.println("value = " + value++);
         assertNotNull(study);
     }
 
@@ -28,6 +30,17 @@ public class StudyLifecycleTest {
     void test2() {
         Study study = new Study(20);
         System.out.println(this);
+        System.out.println("value = " + value++);
+        assertNotNull(study);
+    }
+
+    @Test
+    @Order(3)
+    @Disabled
+    void test_3() {
+        Study study = new Study(30);
+        System.out.println(this);
+        System.out.println("value = " + value++);
         assertNotNull(study);
     }
     // Lifecycle을 추가함으로써 실제로 test1(), test2() 의 인스턴스값이 같음을 확인할 수 있다.
