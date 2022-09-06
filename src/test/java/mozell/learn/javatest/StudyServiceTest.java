05e4b134979a44877aa92db0a4250805bd03a0cf
mozell <mozellatto@gmail.com>
2022-09-06 22:03:35 +0900

    Mock 객체 만들기 (4. @Mock 애노테이션 사용)

--- src/test/java/mozell/learn/javatest/StudyServiceTest.java
+++ src/test/java/mozell/learn/javatest/StudyServiceTest.java
@@ -1,35 +1,49 @@
 package mozell.learn.javatest;
 
-import mozell.learn.mockitoPack.domain.Member;
-import mozell.learn.mockitoPack.domain.Study;
-import mozell.learn.mockitoPack.member.MemberNotFoundException;
 import mozell.learn.mockitoPack.member.MemberService;
 import mozell.learn.mockitoPack.study.StudyRepository;
 import mozell.learn.mockitoPack.study.StudyService;
-import org.junit.Test;
-import org.mockito.Mockito;
-import org.springframework.data.domain.Example;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
-import org.springframework.data.repository.query.FluentQuery;
-
-import java.util.List;
-import java.util.Optional;
-import java.util.function.Function;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.Mockito.mock;
 
+@ExtendWith(MockitoExtension.class)
 public class StudyServiceTest {
 
     @Test
-    public void createStudyService() {
+    @DisplayName("Mockito.mock() 메서드 생성")
+    public void createStudyService_1() {
         MemberService memberService = mock(MemberService.class);
         StudyRepository studyRepository = mock(StudyRepository.class);
 
         StudyService studyService = new StudyService(memberService, studyRepository);
+        assertNotNull(studyService);
+    }
 
+
+
+    @Mock private MemberService memberService;
+    @Mock private StudyRepository studyRepository;
+
+    @Test
+    @DisplayName("@Mock 생성")
+    public void createStudyService_2() {
+        StudyService studyService = new StudyService(memberService, studyRepository);
+        assertNotNull(studyService);
+    }
+
+
+
+    @Test
+    @DisplayName("@Mock 매개변수 생성")
+    public void createStudyService_3(@Mock MemberService memberService,
+                                     @Mock StudyRepository studyRepository) {
+        StudyService studyService = new StudyService(memberService, studyRepository);
         assertNotNull(studyService);
     }
 }
