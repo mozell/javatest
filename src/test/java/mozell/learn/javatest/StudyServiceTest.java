624b56c6674f49cf2edd91a1bef0f36edfdc5bf9
mozell <mozellatto@gmail.com>
2022-09-06 23:17:28 +0900

    Mock 객체 Stubbing 연습문제

--- src/test/java/mozell/learn/javatest/StudyServiceTest.java
+++ src/test/java/mozell/learn/javatest/StudyServiceTest.java
@@ -177,4 +177,29 @@ public class StudyServiceTest {
         });
         assertEquals(Optional.empty(), memberService.findById(3L)); // 3번째 호출
     }
+
+    @Test
+    @DisplayName("Mock 객체 Stubbing 연습문제")
+    public void stubbingTest() {
+
+        StudyService studyService = new StudyService(memberService, studyRepository);
+        assertNotNull(studyService);
+
+        Member member = new Member();
+        member.setId(1L);
+        member.setEmail("test@email.com");
+
+        Study study = new Study(10, "TEST");
+
+        // [TO-DO] memberService 객체에 findById 메소드를 1L 값으로 호출하면, Optional.of(member) 객체를 리턴하도록 Stubbing
+        when(memberService.findById(1L)).thenReturn(Optional.of(member));
+        // [TO-DO] studyRepository 객체에 save 메소드를 study 객체로 호출하면 study 객체 그대로 리턴하도록 Stubbing
+        when(studyRepository.save(study)).thenReturn(study);
+
+        studyService.createNewStudy(1L, study);
+
+        assertNotNull(study.getOwner());
+        assertEquals(member, study.getOwner());
+
+    }
 }
