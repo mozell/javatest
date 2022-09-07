bcbf5c86c70e22da4084d4f3efd8c631ce4cfd3e
mozell <mozellatto@gmail.com>
2022-09-07 21:43:13 +0900

    Mockito BDD 스타일 API

--- src/test/java/mozell/learn/javatest/StudyServiceTest.java
+++ src/test/java/mozell/learn/javatest/StudyServiceTest.java
@@ -8,6 +8,7 @@ import mozell.learn.mockitoPack.study.StudyService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.BDDMockito;
 import org.mockito.InOrder;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
@@ -16,6 +17,8 @@ import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.then;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
@@ -239,5 +242,31 @@ public class StudyServiceTest {
         // [verifyNoMoreInteractions] 더이상 mock 객체를 사용하지 않는지 체크
         verifyNoMoreInteractions(memberService);
 
+    }
+
+    @Test
+    @DisplayName("Mockito BDD 스타일 API")
+    public void mockitoBddApi() {
+
+        //given
+        StudyService studyService = new StudyService(memberService, studyRepository);
+        assertNotNull(studyService);
+
+        Member member = new Member();
+        member.setId(1L);
+        member.setEmail("test@email.com");
+
+        Study study = new Study(10, "TEST");
+
+        given(memberService.findById(1L)).willReturn(Optional.of(member));
+        given(studyRepository.save(study)).willReturn(study);
+
+        //when
+        studyService.createNewStudy(1L, study);
+
+        //then
+        then(memberService).should(times(1)).notify(study);
+        then(memberService).should(times(1)).notify(member);
+        then(memberService).shouldHaveNoMoreInteractions();
     }
 }
