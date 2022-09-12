2d479c2d0ddbfa2434a67f2c22f4e26f72032747
mozell <mozellatto@gmail.com>
2022-09-12 18:37:18 +0900

    ArchUnit JUnit5 연동

---
+++ src/test/java/mozell/learn/javatest/ArchTests.java
@@ -0,0 +1,32 @@
+package mozell.learn.javatest;
+
+import com.tngtech.archunit.junit.AnalyzeClasses;
+import com.tngtech.archunit.junit.ArchTest;
+import com.tngtech.archunit.lang.ArchRule;
+import mozell.learn.mockitoPack.App;
+
+import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.classes;
+import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.noClasses;
+import static com.tngtech.archunit.library.dependencies.SlicesRuleDefinition.slices;
+
+@AnalyzeClasses(packagesOf = App.class)
+public class ArchTests {
+
+    @ArchTest
+    ArchRule domainPackageRule = classes().that().resideInAPackage("..domain..")
+            .should().onlyBeAccessed().byClassesThat()
+            .resideInAnyPackage("..study..", "..member..", "..domain..");
+
+    @ArchTest
+    ArchRule memberPackageRule = noClasses().that().resideInAPackage("..domain..")
+            .should().accessClassesThat().resideInAPackage("..member..");
+
+    @ArchTest
+    ArchRule studyPackageRule = noClasses().that().resideOutsideOfPackage("..study..")
+            .should().accessClassesThat().resideInAPackage("..study..");
+
+    @ArchTest
+    ArchRule freeOfCycles = slices().matching("..mockitoPack.(*)..")
+            .should().beFreeOfCycles();
+
+}
